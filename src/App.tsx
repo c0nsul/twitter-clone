ad00a5ed701e8e8ff0e05a9648e432347f01897a
ConsuL <consul.developer@gmail.com>
2020-10-12 14:34:50 +0300

    add

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from "@material-ui/core/Button";
+import SignIn from "./pages/Signin";
+
+function App() {
+  return (
+    <div className="App">
+        <SignIn/>
+    </div>
+  );
+}
+
+export default App;
