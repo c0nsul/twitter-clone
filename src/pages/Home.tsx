8f93f599fd9d75bca3f66063827066c757efe76c
ConsuL <consul.developer@gmail.com>
2020-10-15 11:51:55 +0300

    home left side styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,70 +1,92 @@
 import React from 'react';
-import {Grid, IconButton, Paper, Typography} from "@material-ui/core";
+import {Grid, IconButton, makeStyles, Paper, Typography} from "@material-ui/core";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import SearchIcon from "@material-ui/icons/Search";
-import NotificationIcon from "@material-ui/icons/Notifications";
+import NotificationIcon from "@material-ui/icons/NotificationsNone";
 import MessageIcon from "@material-ui/icons/EmailOutlined";
 import BookmarkIcon from "@material-ui/icons/BookmarkBorder";
 import ListIcon from "@material-ui/icons/ListAlt";
 import UserIcon from "@material-ui/icons/PermIdentity";
 
+export const useHomeStyles = makeStyles(() => ({
+        sideMenuList: {
+            listStyle: 'none',
+            padding: 0,
+            margin: 0,
+        },
+        sideMenuListItem: {
+            display: 'flex',
+            alignItems: 'center',
+        },
+        sideMenuListItemLabel: {
+            fontWeight:700,
+            fontSize: 20,
+            marginLeft: 15,
+        },
+        sideMenuListItemIcon: {
+            fontSize: 28
+        }
+    })
+);
 
 export const Home = () => {
+    const classes = useHomeStyles();
+
     return (
         <section>
             <Grid container spacing={3}>
                 <Grid item xs={1}>
-                    <ul>
-                        <li>
-                            <IconButton aria-label="delete" color="primary">
-                                <TwitterIcon/>
+                    <ul className={classes.sideMenuList}>
+                        <li className={classes.sideMenuListItem}>
+                            <IconButton aria-label="" color="primary">
+                                <TwitterIcon className={classes.sideMenuListItemIcon} />
                             </IconButton>
                         </li>
-                        <li>
-                            <IconButton aria-label="delete" color="primary">
-                                <SearchIcon/>
+                        <li className={classes.sideMenuListItem}>
+                            <IconButton aria-label="" >
+                                <SearchIcon className={classes.sideMenuListItemIcon} />
                             </IconButton>
-                            <Typography>
+                            <Typography className={classes.sideMenuListItemLabel}>
                                 Search
                             </Typography>
                         </li>
-                        <li>
-                            <IconButton aria-label="delete" color="primary">
-                                <NotificationIcon/>
+                        <li className={classes.sideMenuListItem}>
+                            <IconButton aria-label="" >
+                                <NotificationIcon className={classes.sideMenuListItemIcon} />
                             </IconButton>
-                            <Typography>
-                                Notificataions
+                            <Typography className={classes.sideMenuListItemLabel}>
+                                Notifications
                             </Typography>
                         </li>
-                        <li>
-                            <IconButton aria-label="delete" color="primary">
-                                <MessageIcon/>
+                        <li className={classes.sideMenuListItem}>
+                            <IconButton aria-label="" >
+                                <MessageIcon className={classes.sideMenuListItemIcon} />
                             </IconButton>
-                            <Typography>
+                            <Typography className={classes.sideMenuListItemLabel}>
                                 Messages
                             </Typography>
                         </li>
-                        <li>
-                            <IconButton aria-label="delete" color="primary">
-                                <BookmarkIcon/>
+                        <li className={classes.sideMenuListItem}>
+                            <IconButton aria-label="" >
+                                <BookmarkIcon className={classes.sideMenuListItemIcon} />
                             </IconButton>
-                            <Typography>
-                                Booksmarks
+                            <Typography className={classes.sideMenuListItemLabel}>
+                                Bookmarks
                             </Typography>
                         </li>
-                        <li>
-                            <IconButton aria-label="delete" color="primary">
-                                <ListIcon/>
+                        <li className={classes.sideMenuListItem}>
+                            <IconButton aria-label="" >
+                                <ListIcon className={classes.sideMenuListItemIcon} />
                             </IconButton>
-                            <Typography>
+                            <Typography className={classes.sideMenuListItemLabel}>
                                 List
                             </Typography>
                         </li>
-                        <li>
-                            <IconButton aria-label="delete" color="primary">
-                                <UserIcon/>
+                        <li className={classes.sideMenuListItem}>
+                            <IconButton aria-label="" >
+                                <UserIcon className={classes.sideMenuListItemIcon} />
                             </IconButton>
-                            <Typography>
+                            <Typography className={classes.sideMenuListItemLabel}>
                                 Profile
                             </Typography>
                         </li>
